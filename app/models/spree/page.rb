5bdb97028806277d0bbe3135f8ba8cf9b63a13a6
Adam Mueller <adam@freerunningtech.com>
2014-02-06 16:19:57 -0800

    Bump config/routes to fix issues with Rails 3.2
    Change-Id: I2b3a3cc476257d8d0e43750ac611fbb604f67e7d

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -3,23 +3,33 @@ class Spree::Page < ActiveRecord::Base
 
   validates_presence_of :title
   validates_presence_of [:slug, :body], :if => :not_using_foreign_link?
+  validates_presence_of :layout, :if => :render_layout_as_partial?
 
   scope :visible, where(:visible => true)
   scope :header_links, where(:show_in_header => true).visible
   scope :footer_links, where(:show_in_footer => true).visible
   scope :sidebar_links, where(:show_in_sidebar => true).visible
 
-
   before_save :update_positions_and_slug
+
+  attr_accessible :title, :slug, :body, :meta_title, :meta_keywords, :meta_description, :layout, :foreign_link, :position, :show_in_sidebar, :show_in_header, :show_in_footer, :visible, :render_layout_as_partial
+
+  def self.by_slug(slug)
+    slug = StaticPage::remove_spree_mount_point(slug) unless Rails.application.routes.url_helpers.spree_path == "/"
+    pages = self.arel_table
+    query = pages[:slug].eq(slug).or(pages[:slug].eq("/#{slug}"))
+    self.where(query)
+  end
 
   def initialize(*args)
     super(*args)
+
     last_page = Spree::Page.last
     self.position = last_page ? last_page.position + 1 : 0
   end
 
   def link
-    foreign_link.blank? ? slug_link : foreign_link
+    foreign_link.blank? ? slug : foreign_link
   end
 
 private
@@ -35,7 +45,6 @@ private
     end
 
     if not_using_foreign_link?
-      self.slug = slug_link
       Rails.cache.delete('page_not_exist/' + self.slug)
     end
     return true
@@ -43,13 +52,5 @@ private
 
   def not_using_foreign_link?
     foreign_link.blank?
-  end
-
-  def slug_link
-    ensure_slash_prefix slug
-  end
-
-  def ensure_slash_prefix(str)
-    str.index('/') == 0 ? str : '/' + str
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 module StaticPage
   def self.remove_spree_mount_point(path)
-    regex = Regexp.new Rails.application.routes.named_routes[:spree].path
-    path.sub( regex, '')
+    regex = Regexp.new Rails.application.routes.url_helpers.spree_path
+    path.sub( regex, '').split('?')[0]
   end
 end
 
 class Spree::StaticPage
   def self.matches?(request)
-    path = StaticPage::remove_spree_mount_point(request.fullpath)
-    count = Spree::Page.visible.where(:slug => path).count
-    0 < count
+    slug = StaticPage::remove_spree_mount_point(request.fullpath)
+    pages = Spree::Page.arel_table
+    Spree::Page.visible.by_slug(slug).exists?
   end
 end
 
 class Spree::StaticRoot
   def self.matches?(request)
     path = StaticPage::remove_spree_mount_point(request.fullpath)
-    (path == '/') && Spree::Page.visible.find_by_slug(path)
+    path.nil? && Spree::Page.visible.by_slug(path.to_s).first
   end
 end
 
@@ -25,11 +25,11 @@ Spree::Core::Engine.routes.prepend do
   namespace :admin do
     resources :pages
   end
-  
+
   constraints(Spree::StaticRoot) do
     match '/', :to => 'static_content#show', :via => :get, :as => 'static'
   end
-  
+
   constraints(Spree::StaticPage) do
     match '/*path', :to => 'static_content#show', :via => :get, :as => 'static'
   end
